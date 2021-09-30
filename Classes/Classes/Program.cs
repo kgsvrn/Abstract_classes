047d756d87ee97f7e7fa1ca5b4c801b7b89d31c7
kgsvrn <90982069+kgsvrn@users.noreply.github.com>
2021-09-30 12:57:26 +0300

    For check

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -7,27 +7,6 @@ class Program
     static void Main(string[] args)
     {
         double sumSquare = 0, sumP = 0;
-        
-        /*
-        Figure refFg;
-        Rectangle Re = new Rectangle("Прямоугольник", 2, 3);
-
-        refFg = Re;
-        refFg.Print();
-  
-        refFg.Area();
-     
-        refFg.Area2();
-   
-        Figure ffs;
-        circle cir = new circle("Треугольник", 6);
-        ffs = cir;
-        ffs.Print();
-
-        ffs.Area();
-
-        ffs.Area2();
-        */
         List<Figure> figures = new List<Figure>();
         figures.Add(new circle("Круг1", 2));
         figures.Add(new circle("Круг2", 3));
